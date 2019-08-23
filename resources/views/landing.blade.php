95abcde0e358488453bda5a6a4958b505b1d282a
Chainfailure <chainfailure@protonmail.com>
2019-08-23 13:20:50 +0200

    rephrased, grammar error

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -20,7 +20,7 @@
 
                     <div class="mt-4">
                         <h2>Why did you make this?</h2>
-                        <p>I was fed up with the amount of bullshit in my timeline.</p>
+                        <p>I was fed up with the amount of bullshit on my timeline.</p>
                     </div>
 
                     <div class="mt-4">
@@ -28,8 +28,8 @@
                         <ul class="text-left">
                             <li>You click the button underneath.</li>
                             <li>Twitter will prompt you to authorize us to modify your blocklist.</li>
-                            <li>Once granted access we add, what we believe, are BSV centered accounts to your blocklist.</li>
-                            <li>We remind you to remove our authorization as we're not storing your access keys, thus there is no use for us to remain authorized.</li>
+                            <li>Once we are granted access we add, what we believe, are BSV centered accounts to your blocklist.</li>
+                            <li>We remind you to remove our authorization as we're not storing your access keys. There is no use for them to remain authorized.</li>
                         </ul>
                     </div>
                 </section>
